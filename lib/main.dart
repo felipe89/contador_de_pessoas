96312819e1f1b7b2b6a3edf9e5dd520af4092729
Gilmar Silva <gilmar.silva@reservafacil.tur.br>
2018-10-09 14:15:20 -0300

    atualizar tela app

--- lib/main.dart
+++ lib/main.dart
@@ -3,48 +3,86 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       title: "Contador de Pessoas",
-      home: Stack(
-        children: <Widget>[
-          Image.asset(
-            "images/fundo.jpg",
-            fit: BoxFit.cover,
-            height: 1000.0,
-          ),
-          Column(mainAxisAlignment: MainAxisAlignment.center, children: <
-              Widget>[
-            Text("Pessoa = 0",
-                style: TextStyle(
-                    color: Colors.white, fontWeight: FontWeight.bold)),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child: FlatButton(
-                  child: Text(
-                    "+1",
-                    style: TextStyle(fontSize: 40.0, color: Colors.white),
-                  ),
-                  onPressed: () {},
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child: FlatButton(
-                  child: Text(
-                    "+1",
-                    style: TextStyle(fontSize: 40.0, color: Colors.white),
-                  ),
-                  onPressed: () {},
-                ),
-              ),
-              Text(
-                "Pode Entrar!",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontStyle: FontStyle.italic,
-                    fontSize: 20.0),
-              ),
-            ]),
-          ])
-        ],
-      )));
+      home:Home()));
 }
+class Home extends StatefulWidget {
+
+  @override
+  _HomeState createState() => _HomeState();
+    }
+
+class _HomeState extends State<Home> {
+  String _infoText = "Pode Entrar!";
+  int _people = 0;
+
+  void _changePeople(int delta){
+    setState(() {
+      _people += delta;
+
+      if(_people < 0){
+        _infoText = "Quebrou tudo";
+      }else if (_people <= 10){
+        _infoText = "Estouro mlk";
+      }else {
+        _infoText = "Lotadão!";
+      }
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return  Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/fundo.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+            Widget>[
+          Text("Pessoa = $_people",
+              style: TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.bold)),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: FlatButton(
+                child: Text(
+                  "+1",
+                  style: TextStyle(fontSize: 40.0, color: Colors.blueAccent),
+                ),
+                onPressed: () {
+                  _changePeople(1);
+                  },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: FlatButton(
+                child: Text(
+                  "-1",
+                  style: TextStyle(fontSize: 40.0, color: Colors.blueAccent),
+                ),
+                onPressed: () {
+                  _changePeople(-1);
+                  },
+              ),
+            ),
+            Text(
+              _infoText,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 20.0),
+            ),
+          ]),
+        ])
+      ],
+    );
+  }
+}
+}
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+  }
